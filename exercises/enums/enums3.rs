ded362515cb7818074c1352524be979539c29b2e
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2022-10-14 16:14:28 +0200

    don't want to make these exos again

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,12 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Quit,
+    Echo(String),
+    Move(Point),
+    ChangeColor((u8, u8, u8)),
 }
 
 struct Point {
@@ -39,6 +41,12 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
         // Remember: When passing a tuple as a function argument, you'll need extra parentheses: fn function((t, u, p, l, e))
+        match message {
+            Message::Quit => self.quit(),
+            Message::Move(p) => self.move_position(p),
+            Message::Echo(s) => self.echo(s),
+            Message::ChangeColor(t) => self.change_color(t),
+        };
     }
 }
 
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -9,11 +9,14 @@
 // - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
-
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+ fn calculate_price_of_apples(count: u32) -> u32 {
+    if count > 40 {
+        count
+    } else {
+        count * 2
+    }
+ }
 
 // Don't modify this function!
 #[test]
